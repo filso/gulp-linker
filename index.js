8aa98ea99451199f49517e2acd0f37bc99d448ba
Filip Sobczak <filsob@gmail.com>
2014-05-12 17:55:28 +0100

    feat(matching): add matching files with excludes

--- index.js
+++ index.js
@@ -12,10 +12,36 @@
 var util = require('util');
 var through = require('through2');
 var gutil = require('gulp-util');
-var extend = require("xtend");
-var glob = require("glob");
-var fs = require("fs");
+var extend = require('xtend');
+var glob = require('glob');
+var fs = require('fs');
+var _ = require('lodash');
 var PluginError = gutil.PluginError;
+
+
+// Process specified wildcard glob patterns or filenames against a
+// callback, excluding and uniquing files in the result set.
+var processPatterns = function(patterns, fn) {
+  // Filepaths to return.
+  var result = [];
+  // Iterate over flattened patterns array.
+  _.flatten(patterns).forEach(function(pattern) {
+    // If the first character is ! it should be omitted
+    var exclusion = pattern.indexOf('!') === 0;
+    // If the pattern is an exclusion, remove the !
+    if (exclusion) { pattern = pattern.slice(1); }
+    // Find all matching files for this pattern.
+    var matches = fn(pattern);
+    if (exclusion) {
+      // If an exclusion, remove matching files.
+      result = _.difference(result, matches);
+    } else {
+      // Otherwise add matching files.
+      result = _.union(result, matches);
+    }
+  });
+  return result;
+};
 
 
 module.exports = function(options) {
@@ -29,7 +55,14 @@ module.exports = function(options) {
     relative: false
   }, options);
 
-  var files = glob.sync(options.scripts, {});
+  if (typeof options.scripts === 'string') {
+    options.scripts = [options.scripts];
+  }
+
+  var files = processPatterns(options.scripts, function(pattern) {
+    return glob.sync(pattern, {});
+  });
+
 
   var insertLinks = function(file) {
     var start, end;
